396fa377273f0f21bfb15046f2312b75d3b218bd
pascal <1_976@mail.ru>
2022-09-02 16:20:39 +0400

    Added avg function.

--- src/sum.mjs
+++ src/sum.mjs
@@ -1,9 +1,15 @@
 import * as R from 'ramda';
 
 function sum(...numbers) {
-	console.log('Add function from test submodule!');
+	console.log('Sum function from test submodule!');
 
 	return R.reduce(R.add, 0, numbers);
 }
 
-export default sum;
+function avg(...numbers) {
+	console.log('Avg function from test submodule!');
+
+	return numbers.length === 0 ? 0 : R.reduce(R.add, 0, numbers) / numbers.length;
+}
+
+export { sum, avg };
